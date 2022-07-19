28698af0ceba3149b623c4138767912f3070d59c
Andrew Clemons <andrew.clemons@gmail.com>
2022-07-19 18:27:38 +0900

    Add round trip serialisation test.

--- tests/test_serialise_deserialise.py
+++ tests/test_serialise_deserialise.py
@@ -1,28 +1,28 @@
 import os
-import sys
-import tempfile
-import toml
+import pytest
+from tempfile import NamedTemporaryFile
 
-slackroll_file = os.path.join(os.path.dirname(__file__), "..", "slackroll")
+from slackroll import try_load, try_dump
 
-if sys.version_info[0] <= 2:
-    import imp
-    slackroll = imp.load_source('slackroll', slackroll_file)
-else:
-    from importlib.util import spec_from_loader, module_from_spec
-    from importlib.machinery import SourceFileLoader
+@pytest.fixture
+def blacklist():
+    return ['^\\.\\/(?!chromium|libreoffice|vlc).*$@\\/sbrepos\\/', '^\\./testing.*', 'glibc-(?!zoneinfo)@^(?!http)']
 
-    spec = spec_from_loader("slackroll", SourceFileLoader("slackroll", slackroll_file))
-    slackroll = module_from_spec(spec)
-    spec.loader.exec_module(slackroll)
-
-
-from slackroll import try_load
-
-def test_deserialise():
-    """Checks if we can round deserialise a known good file.."""
+def test_deserialise(blacklist):
+    """Checks if we can deserialise a known good file."""
 
     data_file = os.path.join(os.path.dirname(__file__), "..", "data", "blacklist.db")
-    expected_data = ['^\\.\\/(?!chromium|libreoffice|vlc).*$@\\/sbrepos\\/', '^\\./testing.*', 'glibc-(?!zoneinfo)@^(?!http)']
 
-    assert expected_data == try_load(data_file)
+    assert blacklist == try_load(data_file)
+
+
+def test_round_trip_serialisation(blacklist):
+    """Checks if we can round trip serialise then deserialise a value."""
+
+    f = NamedTemporaryFile(delete=True)
+
+    try_dump(blacklist, f.name)
+
+    assert blacklist == try_load(f.name)
+
+    f.close()
