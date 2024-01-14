64660b16037f7f188b40f156f193b898508e403c
Brent Spillner <spillner@acm.org>
2024-01-14 16:17:16 +1000

    Adjusted prioritised-pkgs tests to use transient_key() instead of transient_cmp()

--- tests/test_prioritised_pkgs.py
+++ tests/test_prioritised_pkgs.py
@@ -2,58 +2,52 @@ import os
 
 import toml
 
-from slackroll import transient_cmp
+from slackroll import transient_key
 
 
 def test_transient_cmp_normal_pkg_same_state():
     left = ("python2", "slackroll_state_outdated")
     right = ("python3", "slackroll_state_outdated")
 
-    assert transient_cmp(left, right) == -1
-    assert transient_cmp(right, left) == 1
-    assert transient_cmp(left, left) == 0
+    assert transient_key(left) < transient_key(right)
+    assert transient_key(left) == transient_key(left)
 
 
 def test_transient_cmp_normal_pkg_different_state():
     left = ("python2", "slackroll_state_new")
     right = ("python3", "slackroll_state_outdated")
 
-    assert transient_cmp(left, right) == 0
-    assert transient_cmp(right, left) == 0
-    assert transient_cmp(left, left) == 0
+    assert transient_key(left) == transient_key(right)
+    assert transient_key(left) == transient_key(left)
 
 
 def test_transient_cmp_prioritised_pkg_aaa_glib_solibs():
     left = ("aaa_glibc-solibs", "slackroll_state_new")
     right = ("python3", "slackroll_state_outdated")
 
-    assert transient_cmp(left, right) == -1
-    assert transient_cmp(right, left) == 1
-    assert transient_cmp(left, left) == 0
+    assert transient_key(left) < transient_key(right)
+    assert transient_key(left) == transient_key(left)
 
 
 def test_transient_cmp_prioritised_pkg_glibc_solibs():
     left = ("glibc-solibs", "slackroll_state_new")
     right = ("python3", "slackroll_state_outdated")
 
-    assert transient_cmp(left, right) == -1
-    assert transient_cmp(right, left) == 1
-    assert transient_cmp(left, left) == 0
+    assert transient_key(left) < transient_key(right)
+    assert transient_key(left) == transient_key(left)
 
 
 def test_transient_cmp_prioritised_pkg_sed():
     left = ("sed", "slackroll_state_new")
     right = ("python3", "slackroll_state_outdated")
 
-    assert transient_cmp(left, right) == -1
-    assert transient_cmp(right, left) == 1
-    assert transient_cmp(left, left) == 0
+    assert transient_key(left) < transient_key(right)
+    assert transient_key(left) == transient_key(left)
 
 
 def test_transient_cmp_prioritised_pkg_pkgtools():
     left = ("pkgtools", "slackroll_state_new")
     right = ("python3", "slackroll_state_outdated")
 
-    assert transient_cmp(left, right) == -1
-    assert transient_cmp(right, left) == 1
-    assert transient_cmp(left, left) == 0
+    assert transient_key(left) < transient_key(right)
+    assert transient_key(left) == transient_key(left)
